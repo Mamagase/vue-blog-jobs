ab5e6157e6aeb1a0d3e4e48b43fbb1887e417558
gift <gn.mamagase@gmail.com>
2023-02-15 08:42:35 +0200

    commits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum fuga explicabo distinctio dicta tempora laudantium qui illo accusamus voluptatem dolorum inventore incidunt, sunt cumque vitae veritatis delectus sapiente nostrum, perspiciatis dolorem! Soluta aperiam voluptatum facilis pariatur at ut quo nesciunt commodi, eveniet alias non, amet quos minus perferendis recusandae totam consequatur? Vitae, sed pariatur! Voluptatem mollitia et officiis? Animi eaque rem veritatis dolorum excepturi? Officia itaque, corrupti rerum, voluptates repellendus dignissimos cum recusandae, quod odio nostrum sit aspernatur tempora perspiciatis saepe reiciendis facere! Tenetur blanditiis repellendus deserunt officia eum quasi atque impedit ducimus quisquam. Quis in illo vitae modi.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum fuga explicabo distinctio dicta tempora laudantium qui illo accusamus voluptatem dolorum inventore incidunt, sunt cumque vitae veritatis delectus sapiente nostrum, perspiciatis dolorem! Soluta aperiam voluptatum facilis pariatur at ut quo nesciunt commodi, eveniet alias non, amet quos minus perferendis recusandae totam consequatur? Vitae, sed pariatur! Voluptatem mollitia et officiis? Animi eaque rem veritatis dolorum excepturi? Officia itaque, corrupti rerum, voluptates repellendus dignissimos cum recusandae, quod odio nostrum sit aspernatur tempora perspiciatis saepe reiciendis facere! Tenetur blanditiis repellendus deserunt officia eum quasi atque impedit ducimus quisquam. Quis in illo vitae modi.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
